29fe360b996335420dd5d61d106d980f0d05b8ab
Dimitar Pavlev <dimitar.pavlev03@gmail.com>
2025-02-08 12:47:40 +0200

    add about page

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
 app.listen(5000, () => {
     console.log("The server is working on http://localhost:5000...");
 });
