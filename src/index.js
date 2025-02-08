466f5fe655a5cff24f62363e67a5c6281bed132d
Dimitar Pavlev <dimitar.pavlev03@gmail.com>
2025-02-08 12:22:12 +0200

    Install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("It works");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("It Works");
+});
+
+app.listen(5000, () => {
+    console.log("The server is working on http://localhost:5000...");
+});
