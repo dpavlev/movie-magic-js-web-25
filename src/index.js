4f1dac7253d64983ac9f0be61122ade525f0ac38
Dimitar Pavlev <dimitar.pavlev03@gmail.com>
2025-02-08 12:49:12 +0200

    add 404 page

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
+app.get("*", (req, res) => {
+    res.render("404");
+});
+
 app.listen(5000, () => {
     console.log("The server is working on http://localhost:5000...");
 });
