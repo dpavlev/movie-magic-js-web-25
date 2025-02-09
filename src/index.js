b2bd57d2ba882528c2fea71760780a1bddc33ac7
Dimitar Pavlev <dimitar.pavlev03@gmail.com>
2025-02-09 23:45:55 +0200

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import handlebars from "express-handlebars";
-
+import mongoose from "mongoose";
 import routes from "./routes.js";
 import showRating from "./helpers/ratingHelper.js";
 
 const app = express();
 
+// db config
+try {
+    const uri = "mongodb://localhost:27017/movie-magic-js-web-25";
+    await mongoose.connect(uri);
+    console.log("DB Connected Successfully");
+} catch (err) {
+    console.log("Cannot connect to DB");
+    console.log(err.message);
+}
+
+// Handlebars config
 app.engine("hbs", handlebars.engine({ extname: "hbs", helpers: { showRating } }));
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
+// Express config
 app.use("/static", express.static("src/public"));
 app.use(express.urlencoded({ extended: false })); //Learn express to parse form data
 
