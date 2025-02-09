13fec77c4f799afec52e10f9f5dd34e9b46149d0
Dimitar Pavlev <dimitar.pavlev03@gmail.com>
2025-02-10 00:17:05 +0200

    Change to take the movies data from DB
    Function "getAll" returns the data as documents and it's parsed (with lean()) in homeController (if needed to use document type in other cases)

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,6 @@
-import movies from "../movies.js";
+// import movies from "../movies.js";
 import { v4 as uuid } from "uuid";
+import Movie from "../models/Movie.js";
 
 function getMovie(movieId) {
     const result = movies.find((movie) => movie.id === movieId);
@@ -15,16 +16,16 @@ function create(movieData) {
 }
 
 function getAll(filter = {}) {
-    let result = movies;
-    if (filter.title) {
-        result = result.filter((movie) => movie.title.toLowerCase().includes(filter.title.toLowerCase()));
-    }
-    if (filter.genre) {
-        result = result.filter((movie) => movie.genre.toLowerCase() === filter.genre.toLowerCase());
-    }
-    if (filter.year) {
-        result = result.filter((movie) => movie.year == filter.year);
-    }
+    let result = Movie.find({});
+    // if (filter.title) {
+    //     result = result.filter((movie) => movie.title.toLowerCase().includes(filter.title.toLowerCase()));
+    // }
+    // if (filter.genre) {
+    //     result = result.filter((movie) => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+    // }
+    // if (filter.year) {
+    //     result = result.filter((movie) => movie.year == filter.year);
+    // }
     return result;
 }
 
