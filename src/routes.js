7bb41c2caca19f367555eea6c7223580e25d91a9
Dimitar Pavlev <dimitar.pavlev03@gmail.com>
2025-02-10 12:52:28 +0200

    Add cast path controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
 import movieController from "./controllers/movieController.js";
+import castController from "./controllers/castController.js";
 
 const routes = Router();
 routes.use(homeController);
 routes.use("/movies", movieController);
+routes.use("/cast", castController);
 
 routes.get("*", (req, res) => {
     res.render("404");
