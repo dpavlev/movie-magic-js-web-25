aca31ffb79381688be7553bcd6ec34018d235025
dpavlev <dimitar.pavlev03@gmail.com>
2025-02-12 15:32:04 +0200

    Add login functionality with jwt generation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,14 +7,25 @@ authController.get("/register", (req, res) => {
     res.render("auth/register");
 });
 
-authController.get("/login", (req, res) => {
-    res.render("auth/login");
-});
-
 authController.post("/register", async (req, res) => {
     const userData = req.body;
     await authService.register(userData);
     res.redirect("/auth/login");
+});
+
+authController.get("/login", (req, res) => {
+    res.render("auth/login");
+});
+
+authController.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await authService.login(email, password);
+    } catch (err) {
+        console.log(err.message);
+        return res.redirect("/404");
+    }
+    res.redirect("/");
 });
 
 export default authController;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,29 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 function register(userData) {
     return User.create(userData);
 }
 
+async function login(email, password) {
+    const user = await User.findOne({ email });
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid || !user) {
+        throw new Error("Invalid email or password");
+    }
+
+    const payload = {
+        id: user.id,
+        email: user.email
+    };
+    const token = jwt.sign(payload, SECRET, { expiresIn: "2h" });
+    return token;
+}
+
 export default {
-    register
+    register,
+    login
 };
